777995725379839dee22ff0750cfef4d977d2902
Dagostini01 <pedro01dagostini@gmail.com>
2024-03-19 17:22:44 -0300

    alteraações com react routes

---
+++ src/app/games/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function GamesLayout({children}:{children: React.ReactNode}) {
+    return(
+        <div>
+            <h1>Games Layout</h1>
+            {children}
+        </div>
+    )
+}
